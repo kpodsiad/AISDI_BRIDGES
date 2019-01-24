4497093937b189f97a6ac66bacaf574a0046bccf
kpodsiad <kamilpodsiadlo44@gmail.com>
2019-01-24 20:14:21 +0100

    done

--- graph.h
+++ graph.h
@@ -18,47 +18,48 @@ private:
 	size_t numberOfEdges = 0;
 	std::vector< std::vector<size_t> > adjacencyList;
 
-	bool DFS(size_t toSkip1, size_t toSkip2)
+	bool isGraphComplete()
+	{
+		return numberOfEdges == (numberOfVertexes*(numberOfVertexes-1))/2;
+	}
+
+	size_t DFS(size_t toSkip1, size_t toSkip2)
 	{
 		bool visited[numberOfVertexes];
 		size_t visitedCount = 0;
+		std::stack< size_t > vertexesStack;
 
 		for(int a=0; a < numberOfVertexes; ++a)
 			visited[a] = false;
 
-		visited[toSkip1] = true;
+		visited[toSkip1] = true; //mark given vertexes as visited
 		visited[toSkip2] = true;
 
-		std::stack< size_t > vertexesStack;
-		size_t currentVertex = 0;
-
+		size_t currentVertex = 0; //make sure we start from proper vertex
 		if(currentVertex == toSkip1)
 			++currentVertex;
 		if(currentVertex == toSkip2)
 			++currentVertex;
 
-		vertexesStack.push(currentVertex);
+		vertexesStack.push(currentVertex); //add first vertex to stack
 
 		while( !vertexesStack.empty() )
 		{
-			++visitedCount;
 			size_t vertex = vertexesStack.top();
 			vertexesStack.pop();
-			if( !visited[vertex] )
+
+			if( !visited[vertex] ) //when vertex wasn't visited
 			{
+				++visitedCount;
 				visited[vertex] = true;
 				for(auto v : adjacencyList[vertex])
-					if( !visited[v] )
+				{
+					if (!visited[v])
 						vertexesStack.push(v);
+				}
 			}
 		}
-
-		for (int i = 0; i < numberOfVertexes ; ++i)
-		{
-			if(!visited[i])
-				return true;
-		}
-		return false;
+		return visitedCount;
 	}
 
 	void findBridges(std::vector<std::pair<size_t ,size_t >> &extensiveBridges)
@@ -79,7 +80,7 @@ private:
 					continue;
 				vertexToSkip2 = v;
 
-				if(DFS(vertexToSkip1, vertexToSkip2))
+				if( DFS(vertexToSkip1, vertexToSkip2) != numberOfVertexes-2)
 					extensiveBridges.emplace_back(std::make_pair(vertexToSkip1, vertexToSkip2));
 			}
 			visited[i] = true;
@@ -93,9 +94,8 @@ public:
 			throw std::logic_error("Given vertexes must be different");
 
 		if(std::find(adjacencyList[vertex1].begin(), adjacencyList[vertex1].end(), vertex2) != adjacencyList[vertex1].end())
-		{
 			throw std::logic_error("Can't add edge because it has been already added");
-		}
+
 		adjacencyList[vertex1].emplace_back(vertex2);
 		adjacencyList[vertex2].emplace_back(vertex1);
 		++numberOfEdges;
@@ -110,21 +110,28 @@ public:
 	void printExtensiveBridges(std::ostream &os)
 	{
 		std::vector< std::pair<size_t ,size_t >> extensiveBridges;
-		findBridges(extensiveBridges);
-		for(auto elem : extensiveBridges)
-			os<<"("<<elem.first<<", "<<elem.second<<")"<<std::endl;
+		if(numberOfVertexes <= 3 && !isGraphComplete())
+			findBridges(extensiveBridges);
+
+		if( !extensiveBridges.empty())
+		{
+			for(auto elem : extensiveBridges)
+				os<<"("<<elem.first<<", "<<elem.second<<")"<<std::endl;
+		}
+		else
+			os<<"Graph doesn't have extensive bridges"<<std::endl;
 	}
 
-	void printEdges()
+	void printEdges(std::ostream &os)
 	{
 		for(int i = 0; i<adjacencyList.size(); ++i)
 		{
-			std::cout<<i<<" : ";
+			os<<i<<" : | ";
 			for(auto vertex : adjacencyList[i])
 			{
-				std::cout<<vertex<<"| ";
+				os<<vertex<<" | ";
 			}
-			std::cout<<"\n";
+			os<<std::endl;
 		}
 	}
 };
--- main.cpp
+++ main.cpp
@@ -10,26 +10,15 @@ int main()
 
 	Graph graph;
 	size_t n, v1, v2;
-//	cin>>n;
-//
-//
-//	while( std::cin >> v1 >> v2 )
-//	{
-//		graph.addEdge(v1, v2);
-//	}
+	cin>>n;
+	graph.setNumberOfVertexes(n);
+	while( std::cin >> v1 >> v2 )
+	{
+		graph.addEdge(v1, v2);
+	}
 
-	graph.setNumberOfVertexes(6);
-	graph.addEdge(0 , 1);
-	graph.addEdge(1 ,2 );
-	graph.addEdge( 1,3 );
-	graph.addEdge( 2,4 );
-	graph.addEdge( 3, 4);
-	graph.addEdge( 0,5);
-	graph.addEdge( 3,5);
 	graph.printExtensiveBridges(std::cout);
-
-
-	graph.printEdges();
+	graph.printEdges(std::cout);
 
 	return 0;
 }
